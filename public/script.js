ff9bdfc6cf18f6ce91c7065cecf2e62216fd12d1
julian <julian.wilhelm@online.de>
2026-06-29 22:03:50 +0200

    Notion-Aufgaben Verbindungserweiterung 1 28.06.2026

--- public/script.js
+++ public/script.js
@@ -18,16 +18,28 @@ function fetch_function(URL1, content, action) {
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    const nextMeetingCard = document.getElementById('ul_toDoList');
+    const toDoList = document.getElementById('ul_toDoList');
 
     const currentDate = new Date();
     const isoDate = currentDate.toISOString().split('T');
     let endpoint = "todolist"
 
     try {
-        fetch_function('http://192.168.178.102:5678/webhook-test/ce2710d4-f1bb-4ce2-85f2-2d81a19c2f20', { day: isoDate, endpoint: endpoint }, 'POST')
-            .then((data) => { console.log('To-Do List data:', data); 
-                
+        fetch_function('http://192.168.178.102:5678/webhook/ce2710d4-f1bb-4ce2-85f2-2d81a19c2f20', { day: isoDate, endpoint: endpoint }, 'POST')
+            .then((data) => {
+                console.log('To-Do List data:', data);
+                data.forEach((task) => {
+                    // 
+                    const listItem = document.createElement('li');
+                    listItem.textContent = task.name;
+                    listItem.dataset.taskId = task.id; // Notion ID as a data attribute
+                    listItem.dataset.property_priority_level = task.property_priority_level; // Notion priority level as a data attribute
+                    listItem.dataset.url = task.url; // Notion url as a data attribute
+                    listItem.classList = "list-group-item"
+                    listItem.style.backgroundColor = "#1a1a1a"
+                    listItem.style.color = "white"
+                    toDoList.appendChild(listItem);
+                });
             })
             .catch((error) => {
                 console.error('Error fetching To-Do List:', error);
